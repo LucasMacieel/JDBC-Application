8a745245abe45d0204a0bce2d70e7b7602b641ac
Lucas Maciel <lucasmacielsousa29901@gmail.com>
2022-02-15 13:01:49 -0300

    Implementing delete

--- src/model/dao/impl/SellerDAOJDBC.java
+++ src/model/dao/impl/SellerDAOJDBC.java
@@ -95,6 +95,28 @@ public class SellerDaoJDBC implements SellerDao{
 	@Override
 	public void deleteById(Integer id) {
 		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"DELETE FROM seller "
+					+ "WHERE Id = ? ",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setInt(1, id);
+			
+			int rows = st.executeUpdate();
+			
+			if(rows == 0) {
+				System.out.println("Id not found");
+			}
+			 
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
